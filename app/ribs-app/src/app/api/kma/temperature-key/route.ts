357e20e751550f6a88d3a2d1b6174fb0f60633d4
goju2k <goju2k@naver.com>
2024-11-14 19:47:53 +0900

    revalidate: 0

--- app/ribs-app/src/app/api/kma/temperature-key/route.ts
+++ app/ribs-app/src/app/api/kma/temperature-key/route.ts
@@ -1,15 +1,12 @@
 import { NextResponse } from 'next/server';
 
-// 60초 캐싱
-export const revalidate = 60;
-
 export async function GET(_request: Request) {
 
   const keyResult = {
     src: '',
     time: '',
   };
-  const data = await (await fetch('https://www.weather.go.kr/wgis-nuri/dfs/list/TMP', { next: { revalidate: 60 } })).json();
+  const data = await (await fetch('https://www.weather.go.kr/wgis-nuri/dfs/list/TMP', { next: { revalidate: 0 } })).json();
   if (data && data.dfsList) {
     const [ item ] = data.dfsList;
     if (item.fctDate) {
