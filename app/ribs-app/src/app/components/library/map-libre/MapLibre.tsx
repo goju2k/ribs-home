faacc2a8856a665a0d776ed8e010324ad0b3d86d
goju2k <86723088+goju2k@users.noreply.github.com>
2025-02-24 13:15:50 +0900

    Update MapLibre.tsx

--- app/ribs-app/src/app/components/library/map-libre/MapLibre.tsx
+++ app/ribs-app/src/app/components/library/map-libre/MapLibre.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import { useEffect, useRef } from 'react';
@@ -11,37 +12,43 @@ export function MapLibre({ type = 'demo' }:{type?:'demo'|'naver';}) {
   const map = useRef<maplibregl.Map>();
   useEffect(() => {
 
-    if (container.current) {
-      
-      let mapInstance: maplibregl.Map;
-      if (type === 'naver') {
-        mapInstance = new NaverMapForLibre(
-          {
+    (async () => {
+
+      if (container.current) {
+        let mapInstance: maplibregl.Map;
+        if (type === 'naver') {
+
+          const { data } = await axios.get('https://nrbe.pstatic.net/styles/basic.json?fmt=png');
+
+          mapInstance = new NaverMapForLibre(
+            {
+              container: container.current, // container id
+              center: [ 127.028104, 37.496837 ], // starting position [lng, lat]
+              zoom: 6, // starting zoom,
+            },
+            { tiles: data.tiles },
+          );
+          
+        } else {
+          mapInstance = new maplibregl.Map({
             container: container.current, // container id
+            style: 'https://demotiles.maplibre.org/style.json', // style URL
             center: [ 127.028104, 37.496837 ], // starting position [lng, lat]
             zoom: 6, // starting zoom,
-          },
-          { mapKey: 'yc2mrw1mz8' },
-        );
-      } else {
-        mapInstance = new maplibregl.Map({
-          container: container.current, // container id
-          style: 'https://demotiles.maplibre.org/style.json', // style URL
-          center: [ 127.028104, 37.496837 ], // starting position [lng, lat]
-          zoom: 6, // starting zoom,
+          });
+        }
+
+        // 스크롤 속도 2배
+        mapInstance.scrollZoom.setWheelZoomRate(0.0015);
+  
+        mapInstance.on('idle', (e) => {
+          console.log('idle', e);
         });
+  
+        map.current = mapInstance;
       }
 
-      // 스크롤 속도 2배
-      mapInstance.scrollZoom.setWheelZoomRate(0.0015);
-  
-      mapInstance.on('idle', (e) => {
-        console.log('idle', e);
-      });
-  
-      map.current = mapInstance;
-
-    }
+    })();
 
     return () => {
       map.current && map.current.remove();
