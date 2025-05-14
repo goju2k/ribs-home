5d7c8e3f8a7f316c6de8418773d5d29cf4b4afd7
goju2k <goju2k@naver.com>
2025-05-14 22:49:16 +0900

    테스트로 체크 시간 변경

--- app/ribs-app/src/app/api/spc-sisul/ydp2/route.ts
+++ app/ribs-app/src/app/api/spc-sisul/ydp2/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export const revalidate = 0;
 
-const regex = /18:00~20:00.*?'(\d{4}-\d{2}-\d{2})'\);\s*">예약가능/g;
+const regex = /12:00~14:00.*?'(\d{4}-\d{2}-\d{2})'\);\s*">예약가능/g;
 export async function GET(_request: Request) {
   
   const { data } = await axios.get<any, AxiosResponse<string>>('https://spc2.y-sisul.or.kr/page/rent/rent.od.list.asp');
