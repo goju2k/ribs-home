8d328a8fa05bb07a3b291fcdb379d536f87eca68
goju2k <goju2k@naver.com>
2025-06-07 19:39:20 +0900

    simple prom

---
+++ app/ribs-app/src/app/api/metrics/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+import { Registry, collectDefaultMetrics } from 'prom-client';
+
+export const revalidate = 0;
+
+const register = new Registry();
+collectDefaultMetrics({ register });
+
+export async function GET(_request: Request) {
+
+  const res = NextResponse.json(await register.metrics());
+  res.headers.set('Content-Type', register.contentType);
+  return res;
+}
