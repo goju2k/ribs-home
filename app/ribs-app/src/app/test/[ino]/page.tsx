8bbfc7d866dec8e22e649744ef985d0a8fabe810
goju2k <86723088+goju2k@users.noreply.github.com>
2025-09-03 15:53:06 +0900

    Update page.tsx

--- app/ribs-app/src/app/test/[ino]/page.tsx
+++ app/ribs-app/src/app/test/[ino]/page.tsx
@@ -1,8 +1,8 @@
-import { notFound, permanentRedirect, RedirectType } from 'next/navigation';
+import { permanentRedirect, RedirectType } from 'next/navigation';
 
 export default async function FriendEventPage({ params }:{params:{ino:string;};}) {
 
-  const targetUrl = https://d3vgce7md3dv5f.cloudfront.net/event/sample3.html?ino=${params.ino};
+  const targetUrl = `https://d3vgce7md3dv5f.cloudfront.net/event/sample3.html?ino=${params.ino}`;
   permanentRedirect(targetUrl, RedirectType.replace);
   
 }
