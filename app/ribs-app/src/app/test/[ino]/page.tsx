13c215fc7ba190a249ec5638268bed4c951da595
goju2k <86723088+goju2k@users.noreply.github.com>
2025-09-03 15:48:59 +0900

    Create page.tsx

---
+++ app/ribs-app/src/app/test/[ino]/page.tsx
@@ -0,0 +1,8 @@
+import { notFound, permanentRedirect, RedirectType } from 'next/navigation';
+
+export default async function FriendEventPage({ params }:{params:{ino:string;};}) {
+
+  const targetUrl = https://d3vgce7md3dv5f.cloudfront.net/event/sample3.html?ino=${params.ino};
+  permanentRedirect(targetUrl, RedirectType.replace);
+  
+}
