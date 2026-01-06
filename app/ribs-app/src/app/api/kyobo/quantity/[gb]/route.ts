4a4a7ae1617932bd8c2f2262b9535e269b65bef6
goju2k <goju2k@naver.com>
2026-01-06 21:06:14 +0900

    yp added

--- app/ribs-app/src/app/api/kyobo/quantity/[gb]/route.ts
+++ app/ribs-app/src/app/api/kyobo/quantity/[gb]/route.ts
@@ -4,18 +4,33 @@ import { NextResponse } from 'next/server';
 export const revalidate = 0;
 
 // let cnt = 0;
-export async function GET(_request: Request) {
+export async function GET(_request: Request, { params }: { params: Promise<{ gb: string; }>; }) {
   
-  const { data } = await axios.get<unknown, AxiosResponse<{data:[];}>>('https://product.kyobobook.co.kr/api/gw/pdt/product/S000218735457/location-inventory', { headers: { 'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Mobile Safari/537.36' } });
-  
-  // for test
-  // const data = { data: cnt === 0 ? a : b };
+  const { gb } = (await params);
 
-  // cnt += 1;
+  const result = gb === '2' ? await fetchYoungpung() : await fetchKyobo();
 
-  console.log('data', `${data}`);
-  return NextResponse.json(data.data);
+  console.log('data', `${result}`);
+  return NextResponse.json(result);
 }
+
+const fetchKyobo = async () => {
+  const { data } = await axios.get<unknown, AxiosResponse<{data:[];}>>('https://product.kyobobook.co.kr/api/gw/pdt/product/S000218735457/location-inventory', { headers: { 'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Mobile Safari/537.36' } });
+  return data.data;
+};
+
+const fetchYoungpung = async () => {
+  const { data } = await axios.get<unknown, AxiosResponse<{data:{labst:number;werksNm:string;}[];}>>('https://m.ypbooks.co.kr/back_shop/base_shop/api/v1/product/stock-info?iBookCd=101394998&iNorPrc=15000&iGubun=y', { headers: { 'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Mobile Safari/537.36' } });
+  return [
+    {
+      strAreaGrpCode: '001',
+      list: data.data.map((item) => ({
+        strName: item.werksNm,
+        realInvnQntt: item.labst,
+      })),
+    },
+  ];
+};
 
 // const a = [
 //   {
